7145c4e1395ac652d7bc0dc7f7bac7e721c542c7
Madu <duymanh1697@gmail.com>
2024-09-22 20:35:39 +0700

    commit code first

---
+++ prj-springboot/src/main/java/com/example/prj_springboot/PrjSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.prj_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjSpringbootApplication.class, args);
+	}
+
+}
---
+++ prj-springboot/src/test/java/com/example/prj_springboot/PrjSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prj_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
